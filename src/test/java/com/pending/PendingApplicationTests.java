b329d0b708d4e000f431bd44df6d56914a391f22
Julio <jdelacruz94@msn.com>
2020-12-29 12:22:22 +0100

    first commit

---
+++ src/test/java/com/pending/PendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
